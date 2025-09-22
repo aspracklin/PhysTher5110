82504517fef27c7a9206a257219262dde4c6d5cb
aspracklin <aspracklin@u.northwestern.edu>
2025-09-22 08:40:27 -0500

    finish lab 3

--- scripts/R/lab_03_wrangling_data.R
+++ scripts/R/lab_03_wrangling_data.R
@@ -2,7 +2,10 @@ library(tidyverse) # data formatting and graphing tools
 
 
 # 2.0. Wrangling Data 
-setwd("../") # back out from EEG_sub_file to data file
+if (getwd()=='/Users'){
+  setwd('annaspracklin/Documents/GitHub/PhysTher5110/data')
+}
+# setwd("../") # back out from EEG_sub_file to data file
 list.files()
 
 
@@ -19,16 +22,67 @@ DATA_prob12 <- DATA %>% filter(subID=="oa02" | subID=="oa03")
 DATA_prob12 <- DATA_prob12 %>% filter(Hz<5)
 
 # Create new variables to calculate avg power at each frequency in Frontal, Central, Parietal, and Occipital Regions
-DATA_prob13 <- DATA
-DATA_prob13$Frontal = (DATA_prob13$F3 + DATA_prob13$F7 + DATA_prob13$Fz + DATA_prob13$F4 + DATA_prob13$F8)/5
-DATA_prob13$Central = (DATA_prob13$C3 + DATA_prob13$C4 + DATA_prob13$Cz)/3
-DATA_prob13$Parietal = (DATA_prob13$P3 + DATA_prob13$P7 + DATA_prob13$Pz + DATA_prob13$P4 + DATA_prob13$P8)/5
-DATA_prob13$Occipital = (DATA_prob13$O1 + DATA_prob13$O2 + DATA_prob13$Oz)/3
+df13<- data.frame(matrix(ncol = 2, nrow = 512)) # initialize a data frame
+names(df13) = c("Hz","Frontal") # set variable names
+Frontal <- DATA %>%
+  group_by(Hz) %>% # group by frequency to take mean across people
+  summarise(mean_value = mean(c(F3, F7, Fz, F4, F8), na.rm = TRUE)) %>% # take mean
+  ungroup()
+df13$Frontal <- Frontal$mean_value # assign to data frame
+df13$Hz <- Frontal$Hz # assign frequencies for future plotting
+# Repeat for central, parietal, and occipital regions
+Central <- DATA %>% 
+  group_by(Hz) %>%
+  summarise(mean_value = mean(c(C3, C4, Cz), na.rm = TRUE)) %>%
+  ungroup()
+df13$Central <- Central$mean_value
+Parietal <- DATA %>%
+  group_by(Hz) %>%
+  summarise(mean_value = mean(c(P3, P7, Pz, P4, P8), na.rm = TRUE)) %>%
+  ungroup()
+df13$Parietal <- Parietal$mean_value
+Occipital <- DATA %>%
+  group_by(Hz) %>%
+  summarise(mean_value = mean(c(O1, O2, Oz), na.rm = TRUE)) %>%
+  ungroup()
+df13$Occipital <- Occipital$mean_value
 
+# Create new variables that reflect the natural log of power in the Frontal, Central, 
+# Parietal, and Occipital regions. Also create a new variable for the natural log 
+# transform of frequency so that you can look at the data in both “original” space 
+# (Power ~ Hz) and log-log space (ln(Power) ~ ln(Hz). Save your new data frame in the 
+# “data” folder with the name “data_processed_EEG”. 
 
+df13$logFrontal = log(df13$Frontal)
+df13$logCentral = log(df13$Central)
+df13$logParietal = log(df13$Parietal)
+df13$logOccipital = log(df13$Occipital)
+df13$logHz = log(df13$Hz)
 
+write.csv(df13,file="data_processed_EEG.csv")
 
+plot(df13$logHz,df13$logFrontal,'l',
+     main = "Frontal Region", xlab = "log frequency", ylab = "log power")
+plot(df13$logHz,df13$logCentral,'l',
+     main = "Central Region", xlab = "log frequency", ylab = "log power")
+plot(df13$logHz,df13$logParietal,'l',
+     main = "Parietal Region", xlab = "log frequency", ylab = "log power")
+plot(df13$logHz,df13$logOccipital,'l',
+     main = "Occipital Region", xlab = "log frequency", ylab = "log power")
 
+# Filter out <30 Hz
+df16 <- df13 %>% filter(Hz<=30)
+# Plot
+plot(df16$logHz,df16$logFrontal,'l',
+     main = "Frontal Region", xlab = "log frequency", ylab = "log power")
+plot(df16$logHz,df16$logCentral,'l',
+     main = "Central Region", xlab = "log frequency", ylab = "log power")
+plot(df16$logHz,df16$logParietal,'l',
+     main = "Parietal Region", xlab = "log frequency", ylab = "log power")
+plot(df16$logHz,df16$logOccipital,'l',
+     main = "Occipital Region", xlab = "log frequency", ylab = "log power")
+
+#### written own code above, provided code below ####
 # selecting specific columns
 head(DATA)
 
